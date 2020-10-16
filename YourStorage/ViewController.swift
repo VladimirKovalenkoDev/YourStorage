f2b3d24c30f0e54e753c1852aff482e5229a87a5
vladimir <vovkazon@mail.ru>
2020-10-16 15:30:39 +0300

    Initial Commit

---
+++ YourStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourStorage
+//
+//  Created by Владимир Коваленко on 16.10.2020.
+//  Copyright © 2020 Vladimir Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

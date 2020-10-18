9daeecb8f4a7eb58554c1c0c60c4543e8d24013b
vladimir <vovkazon@mail.ru>
2020-10-18 22:48:55 +0300

    added photo

--- YourStorage/Controllers/ThingsViewController.swift
+++ YourStorage/Controllers/ThingsViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import CoreData
 
-class ThingsViewController: UIViewController {
-//var palceString = ""
+class ThingsViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
-   // @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var thingImage: UIImageView!
     @IBOutlet weak var thingNameTextField: UITextField!
     @IBOutlet weak var thingLabel: UILabel!
     let context = C.context
@@ -21,14 +20,30 @@ class ThingsViewController: UIViewController {
         thingNameTextField.delegate = self
     }
     
+    @IBAction func cameraAccess(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let cameraView = UIImagePickerController()
+            cameraView.delegate = self
+            cameraView.sourceType = .camera
+            self.present(cameraView, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as? UIImage{
+            thingImage.contentMode = .scaleToFill
+            thingImage.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func saveButtonPressed(_ sender: UIButton ) {
         let newThing = InBox(context: context)
+        let imageData = thingImage.image?.pngData()
         let thing = thingNameTextField.text ?? "Thing1"
         newThing.things = thing
+        newThing.photo = imageData
         do {
             print("thing saved")
-            print(newThing.things)
            try  context.save()
         } catch {
         print("error saving context: \(error)/ThingsVC")
